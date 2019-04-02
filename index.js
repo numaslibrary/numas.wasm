9166ab257bfdae79d5dbe460a456446ba9f7d0f9
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-02 21:28:09 +0200

    Modify package

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const d = require('./src/dispatcher')
 const w = require('./src/processor')
 
-d.setProcessor(w)
-d.setOutput('./output')
-d.dispatchAll('./templates')
+const build = (input, output) => {
+    d.setProcessor(w)
+    d.setOutput(output)
+
+    d.dispatchAll(input)
+}
+
+if (require.main === module) {
+    build('./templates', './output')
+}
+
+
+module.exports = {
+    build,
+}
